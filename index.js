f3813dc1622ba82d2ea8b5942bd6a2ca5a3f881d
user <user@MacBook-Pro-user.local>
2024-10-06 00:42:34 +0300

    Add index.html

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const http = require('http')
+import * as http from 'node:http'
+import * as fs from 'node:fs'
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
-  // Set the response HTTP header with HTTP status and content type
   res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
+  res.setHeader('Content-Type', 'text/html')
 
-  // Send the response body "Hello, World!"
-  res.end('Hello, World!\n')
+  fs.readFile('./index.html', (err, data) => {
+    if (err) res.end()
+    res.end(data)
+  })
 })
 
 // Set the port and hostname
-const hostname = '127.0.0.1'
-const port = 80
-
+const hostname = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 80
 // Start the server
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
