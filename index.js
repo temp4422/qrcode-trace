8d380bf40f43eb5efc2da11beaa5bcb7ea15d468
user <user@MacBook-Pro-user.local>
2024-10-06 09:51:30 +0300

    Refactor

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import * as http from 'node:http'
 import * as fs from 'node:fs'
 
-// Create an HTTP server
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
@@ -12,10 +11,8 @@ const server = http.createServer((req, res) => {
   })
 })
 
-// Set the port and hostname
-const hostname = process.env.HOST || '0.0.0.0'
-const port = process.env.PORT || 80
-// Start the server
+const hostname = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 8080 // proxy to nginx 80 and 443 with certbot tls
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
 })
