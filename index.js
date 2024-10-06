a504680a19a7ed2de4e8778f348b645d9dfbe654
user <user@MacBook-Pro-user.local>
2024-10-06 12:21:13 +0300

    Add logging to logs.txt

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import * as http from 'node:http'
 import * as fs from 'node:fs'
+import { Console } from 'console'
 
+// Log to file
+const filePath = './logs.txt'
+if (!fs.existsSync(filePath)) {
+  fs.writeFile(filePath, new Date().toString(), (err) => err)
+}
+const fconsole = new Console({ stdout: fs.createWriteStream(filePath, { flags: 'a' }) })
+
+// Run server
 const server = http.createServer((req, res) => {
+  // Log request
+  let userData = `
+${req.socket.remoteAddress}
+/* === USER HEADERS === */
+${JSON.stringify(req.headers)}`
+  fconsole.log(userData)
+
+  // Response headers
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
 
@@ -11,6 +28,7 @@ const server = http.createServer((req, res) => {
   })
 })
 
+// Default
 const hostname = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 8080 // proxy to nginx 80 and 443 with certbot tls
 server.listen(port, hostname, () => {
